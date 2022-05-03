670a1c042a0062250798aed326a05fe565aa67c7
Ederick Fernandez <edericklf@gmail.com>
2022-05-02 22:35:52 -0700

    Set up backend message transfer to all connected users

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,42 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
+
 function App() {
 
   // notice that we pass a callback function to initialize the socket
   // we don't need to destructure the 'setSocket' function since we won't be updating the socket state
   const [socket] = useState(() => io(':8000'));
-  const [messages, setMessages] = useState([]);
+  const [chat, setChat] = useState([]);
+  
+  const [message, setMessage] = useState("")
 
+  
   useEffect(() => {
-    socket.on("new_message_from_server", msg => 
-      setMessages(prevMessages => {
-        return [msg, ...prevMessages];
+
+    //socket.emit GET
+    socket.on("chat", chat =>
+      setChat(prevMessages => {
+        return [chat, ...prevMessages];
       })
-    )
-  },[])
+    );
+
+  }, []);
  
+
+  const MessageHandler = (e) =>{
+    e.preventDefault()
+    //socket.emit POST
+    socket.emit("message", message)
+    
+  }
+
   // useEffect(() => {
 
   //   // we need to set up all of our event listeners
   //   // in the useEffect callback function
   //   console.log('Is this running?');
-    
+
   //   // note that we're returning a callback function
   //   // this ensures that the underlying socket will be closed if App is unmounted
   //   // this would be more critical if we were creating the socket in a subcomponent
@@ -31,7 +46,17 @@ function App() {
 
   return (
     <div className="App">
+
       <h1>Socket Test</h1>
+
+      {JSON.stringify(chat)}
+      <form onSubmit={MessageHandler}>
+        <input onChange={(e) => setMessage(e.target.value)} value={ message }/>
+        <button>Send</button>
+      </form>
+
+      
+
     </div>
   );
 }
