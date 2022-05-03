f6f0bf9359a53d9019ec522c8636191588d60708
Ederick Fernandez <edericklf@gmail.com>
2022-05-02 22:36:15 -0700

    recommit

--- server/server.js
+++ server/server.js
@@ -3,25 +3,26 @@ const express = require('express')
 const app = express();
 
 const server = app.listen(8000, () =>
-    console.log("Server Port : 800")
+    console.log("Server Port : 8000")
 )
 
 // To initialize the socket, we need to
 // invoke the socket.io library
 // and pass it our Express server
-const io = require('socket.io')(server, { cors: true });
+const io = require('socket.io')(server, { cors: true } );
 
 
 io.on('connection', socket => {
-    console.log("Hand Shake Complete")
+    console.log("Nice to meet you. (HandShake)");
+
     socket.emit("Welcome to the room!")
 
-    //each clent connected gets a unique ID
-    // console.log(socket.id);
+    //socket.on GET
+    socket.on("message", data => {
+        //Sends message to all clients EXCEPT sender
+        // socket.broadcast.emit("chat", data);
 
-    //socket.broadcast will emit to all other clients besides the
-    //client who is actually emitting
-    socket.on("event_from_client", data => {
-        socket.broadcast.emit("send_data_to_all_other_clients", data);
+        socket.emit("chat", data)
+
     })
 })
